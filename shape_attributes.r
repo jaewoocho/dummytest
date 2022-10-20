4cf3aba7ac5f02d42f48433227a3383778f91128
Jaewoocho-VU <jcho02@syr.edu>
2022-10-20 13:17:35 -0500

    Initial commit

---
+++ shape_attributes.r
@@ -0,0 +1,14 @@
+# DS 5380/5384 - Data Science Teamwork in Practice/Leading Data Science Teams
+# Practicing GitHub Flow
+
+## In this r script, you will calculate the area/surface area of a sphere and circle of radius of size r.
+
+r = 3
+
+## Define your functions to calculate the attributes below:
+
+### Area functions
+
+#### print your results here:
+cat("area of circle:", function_name(r))
+cat("area of sphere:", function_name(r))
